04f7ad3df7721bf1cc5e3434bc3cdfc5f2905df4
Tarique-dataEngineer <tariqueisrar7834@gmail.com>
2024-01-13 15:53:45 +0530

    model trainer and hyperparameter tuning

--- src/exception.py
+++ src/exception.py
@@ -1,4 +1,5 @@
 import sys
+sys.path.append('E:/mlprojects')
 from src.logger import logging 
 
 def error_message_detail(error,error_detail:sys):
@@ -7,7 +8,7 @@ def error_message_detail(error,error_detail:sys):
     error_message="Error occured in python script name [{0}] line number [{1}] and error message is [{2}]".format(
         file_name,exc_tb.tb_lineno,str(error))
 
-return error_message 
+    return error_message 
     
 
     
@@ -19,11 +20,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-    
-if __name__=='__main__':
-
-    try: 
-        a=1/0
-    except Exception is e:
-        logging.info("Divided by zero")
-        raise CustomException(e,sys)       
